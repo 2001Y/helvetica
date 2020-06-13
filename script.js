d7753dfeca9fa7fb701c85cff35c1003a34405b3
2001Y <2001y@2001Ys-MBP2019.local>
2020-06-14 02:17:11 +0900

    6.14 2:17

--- script.js
+++ script.js
@@ -1,59 +1,16 @@
-*{
-  padding: 0;
-  margin: 0;
-  box-sizing: border-box;
-}
-body {
-  font-family: Helvetica,游ゴシック体;
-  -ms-scroll-snap-points-y: repeat(100vh);
-  -ms-scroll-snap-type: y mandatory;
-      scroll-snap-type: y mandatory;
-}
-header,section,footer {
-  scroll-snap-align: start;
-  height: 100vh;
-  padding: 5rem;
-  position: relative;
-  display: flex;
-  flex-flow: column;
-  justify-content: space-between;
-}
-header,footer{
-  justify-content: flex-end;
-}
-#one {
-  background: #000;
-  color: #FFF;
-}
-
-span {
-  font-size: .3em;
-  display: block;
-}
-h1,h2,h4 {
-  line-height: 1em;
-}
-h1,h4 {
-  font-size: 8vw;
-}
-h2 {
-  font-size: 4vw;
-}
-ul {
-  list-style: none;
-  display: flex;
-}
-li {
-  margin-right: 1rem;
-}
-p {
-  opacity: .5;
-}
-
-.pen {
-  height: 90vh;
-  position: fixed;
-  right: 0;
-  bottom: 0;
-  z-index: 999999;
-}
+let target = document.querySelectorAll(".observer"),
+    options = {threshold:.5},
+    observer = new IntersectionObserver(
+      function(e){
+        e.forEach(
+          function(e){
+            if (e.isIntersecting){
+              document.body.id = e.target.id+"-body";
+            }
+          }
+        )
+      },options
+    );
+target.forEach(function(e){
+  observer.observe(e);
+});
